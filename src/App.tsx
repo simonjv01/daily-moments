79a9fa9e055a32c0445284e23a15b3331475fe5c
Simon Vargas <sjvargas01@gmail.com>
2021-02-27 14:47:38 -0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import {
+  IonApp, IonRouterOutlet,
+} from '@ionic/react';
+import { IonReactRouter } from '@ionic/react-router';
+import React from 'react';
+import { Redirect, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import SettingsPage from './pages/SettingsPage';
+
+const App: React.FC = () => {
+  return (
+    <IonApp>
+     <IonReactRouter>
+       <IonRouterOutlet>
+      <Route exact path="/home">
+        <HomePage />
+      </Route>
+      <Route exact path="/settings">
+        <SettingsPage />
+      </Route>
+      <Redirect exact path="/" to="/home" />
+      </IonRouterOutlet>
+     </IonReactRouter>
+    </IonApp>
+  );
+};
+
+export default App;
