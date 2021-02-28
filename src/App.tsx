b43def6c56e476f5b0be28ad5af7ecb312afba9e
Simon Vargas <sjvargas01@gmail.com>
2021-02-28 00:29:42 -0500

    created LoginPage

--- src/App.tsx
+++ src/App.tsx
@@ -3,38 +3,48 @@ import {
 } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
 import { home as homeIcon, settings as settingsIcon  } from 'ionicons/icons';
-import React from 'react';
+import React, { useState } from 'react';
 import { Redirect, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import SettingsPage from './pages/SettingsPage';
 import EntryPage from './pages/EntryPage';
+import LoginPage from './pages/LoginPage';
 
 const App: React.FC = () => {
+  const [loggedIn, setLoggedIn] = useState(false);
+  console.log(`rendering App with loggedIn=${loggedIn}`);
   return (
     <IonApp>
      <IonReactRouter>
        <IonTabs>
        <IonRouterOutlet>
-      <Route exact path="/entries">
-        <HomePage />
+       <Route exact path="/login">
+        
+        <LoginPage loggedIn={loggedIn} 
+        onLogin={() => setLoggedIn(true)}
+        />
+        
       </Route>
-      <Route exact path="/entries/:id">
+      <Route exact path="/my/entries">
+        {loggedIn ? <HomePage /> : <Redirect to="/login" />}
+      </Route>
+      <Route exact path="/my/entries/:id">
         <EntryPage />
       </Route>
-      <Route exact path="/settings">
+      <Route exact path="/my/settings">
         <SettingsPage />
       </Route>
-      <Redirect exact path="/" to="/entries" />
+      <Redirect exact path="/" to="/my/entries" />
       </IonRouterOutlet>
 
       {/** Bottom navigation bar */}
 
       <IonTabBar slot="bottom">
-        <IonTabButton tab="home" href="/entries">
+        <IonTabButton tab="home" href="/my/entries">
           <IonIcon icon={homeIcon} />
           <IonLabel>Home</IonLabel>
         </IonTabButton>
-        <IonTabButton tab="settings" href="/settings">
+        <IonTabButton tab="settings" href="/my/settings">
           <IonIcon icon={settingsIcon} />
           <IonLabel>Settings</IonLabel>
         </IonTabButton>
