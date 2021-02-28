aa39d9b0256d7a14ae82f1d569589fc765fd1e2d
Simon Vargas <sjvargas01@gmail.com>
2021-02-28 08:51:42 -0500

    created AppTabs page

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
 import {
-  IonApp, IonIcon, IonLabel, IonRouterOutlet, IonTabBar, IonTabButton, IonTabs,
+  IonApp, IonRouterOutlet, 
 } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
-import { home as homeIcon, settings as settingsIcon  } from 'ionicons/icons';
 import React, { useState } from 'react';
 import { Redirect, Route } from 'react-router-dom';
-import HomePage from './pages/HomePage';
-import SettingsPage from './pages/SettingsPage';
-import EntryPage from './pages/EntryPage';
+import AppTabs from '../src/AppTabs';
 import LoginPage from './pages/LoginPage';
 
 const App: React.FC = () => {
@@ -16,40 +13,17 @@ const App: React.FC = () => {
   return (
     <IonApp>
      <IonReactRouter>
-       <IonTabs>
        <IonRouterOutlet>
        <Route exact path="/login">
-        
         <LoginPage loggedIn={loggedIn} 
         onLogin={() => setLoggedIn(true)}
         />
-        
       </Route>
-      <Route exact path="/my/entries">
-        {loggedIn ? <HomePage /> : <Redirect to="/login" />}
-      </Route>
-      <Route exact path="/my/entries/:id">
-        <EntryPage />
-      </Route>
-      <Route exact path="/my/settings">
-        <SettingsPage />
+      <Route path="/my">
+        <AppTabs loggedIn={loggedIn} />
       </Route>
       <Redirect exact path="/" to="/my/entries" />
       </IonRouterOutlet>
-
-      {/** Bottom navigation bar */}
-
-      <IonTabBar slot="bottom">
-        <IonTabButton tab="home" href="/my/entries">
-          <IonIcon icon={homeIcon} />
-          <IonLabel>Home</IonLabel>
-        </IonTabButton>
-        <IonTabButton tab="settings" href="/my/settings">
-          <IonIcon icon={settingsIcon} />
-          <IonLabel>Settings</IonLabel>
-        </IonTabButton>
-      </IonTabBar>
-      </IonTabs>
      </IonReactRouter>
     </IonApp>
   );
